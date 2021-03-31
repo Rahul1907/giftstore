4f4bf9b2f9c71ae0a02ab9185b578dbc44c8c533
shital@zestard.net <shital@zestard.net>
2021-03-31 22:44:22 +0530

    Updates

---
+++ precache-manifest.f33c2c40402a50959f83c26e2b4043a0.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7e33128f737e33b5f076134873f5b03",
+    "url": "/giftstore/index.html"
+  },
+  {
+    "revision": "2bdc62d90b61640870e5",
+    "url": "/giftstore/static/css/2.9b8bb02f.chunk.css"
+  },
+  {
+    "revision": "c453be65768819da8aa4",
+    "url": "/giftstore/static/css/main.f6382a32.chunk.css"
+  },
+  {
+    "revision": "2bdc62d90b61640870e5",
+    "url": "/giftstore/static/js/2.1704434c.chunk.js"
+  },
+  {
+    "revision": "c04d04844d4b3c3c22d1625cccf81ac6",
+    "url": "/giftstore/static/js/2.1704434c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c453be65768819da8aa4",
+    "url": "/giftstore/static/js/main.cff1bb41.chunk.js"
+  },
+  {
+    "revision": "6cc712b1fa3734dc737b",
+    "url": "/giftstore/static/js/runtime-main.59f92d0d.js"
+  },
+  {
+    "revision": "e7c7b001d27b5b38c51b53c4c51eeee9",
+    "url": "/giftstore/static/media/BlackPack.e7c7b001.jpg"
+  },
+  {
+    "revision": "c216d7285ae91b7331eb1e9f687103b4",
+    "url": "/giftstore/static/media/WhitePack.c216d728.jpg"
+  },
+  {
+    "revision": "765dac12ca14be6d5c2ba8be895491b3",
+    "url": "/giftstore/static/media/gift1.765dac12.jpg"
+  },
+  {
+    "revision": "3fda7961957b851843c04ebd3e84caaf",
+    "url": "/giftstore/static/media/gift2.3fda7961.jpg"
+  },
+  {
+    "revision": "536c9bdf3fb6b08cd8ee159f85b9e6e0",
+    "url": "/giftstore/static/media/gift3.536c9bdf.jpg"
+  },
+  {
+    "revision": "c6e76ec6ec15a7213850e3be3e3cdce0",
+    "url": "/giftstore/static/media/gift4.c6e76ec6.jpg"
+  },
+  {
+    "revision": "18811cc8d6db46ab8bcd4a51a674a645",
+    "url": "/giftstore/static/media/gift5.18811cc8.jpg"
+  },
+  {
+    "revision": "6529564400f6ea3c87cb31e14fa55fd7",
+    "url": "/giftstore/static/media/sticker1.65295644.jpg"
+  },
+  {
+    "revision": "d0dbd1f7139b936b2e04b61626274c38",
+    "url": "/giftstore/static/media/sticker2.d0dbd1f7.jpeg"
+  },
+  {
+    "revision": "4c99dd7fb0cda199c10b66025df0e0f6",
+    "url": "/giftstore/static/media/sticker3.4c99dd7f.png"
+  },
+  {
+    "revision": "f5e7fd9d17ceba4f36eb55965dbfd07d",
+    "url": "/giftstore/static/media/title.f5e7fd9d.jpg"
+  }
+]);
--- src/Components/Reducers/rootreducers.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux'
-import bagReducers from './bagreducer'
-
-const rootreducers=combineReducers({
-  gifts:bagReducers
-})
-
-export default rootreducers
